cc25103a5ccf786939fe4718b2aa2e28a0191b7a
Swapnil Bhong <99287395+DjBoyswapnil@users.noreply.github.com>
2024-06-06 23:24:37 +0530

    Add files via upload

---
+++ ExamPortal/src/main/java/com/examportal/exam/Controllar/PaperController.java
@@ -0,0 +1,48 @@
+package com.examportal.exam.Controllar;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examportal.exam.Entity.Paper;
+import com.examportal.exam.model.PaperDTO;
+import com.examportal.exam.service.PaperService;
+import com.examportal.exam.util.PaperConverter;
+
+@RestController
+@RequestMapping("/api")
+public class PaperController
+{
+	@Autowired
+	private PaperService paperService;
+	
+	@Autowired
+	private PaperConverter paperConverter;
+	
+	@PostMapping("/CreatePaper")
+	ResponseEntity<PaperDTO>CreatePaper(@Validated @RequestBody PaperDTO paperDTO)
+	{
+		final Paper paper=paperConverter.ConvertPaperEntity(paperDTO);
+		return new ResponseEntity<PaperDTO>(paperService.CreatePaper(paper),HttpStatus.CREATED) ;
+	}
+	@GetMapping("/GetAllPaper")
+	List<PaperDTO> GetAllPaper()
+	{
+		return paperService.GetAllPaper();
+	}
+	@DeleteMapping("/DeletePaperDetails/{pid}")
+	String DeletePaperDetails(@PathVariable("pid") int id)
+	{
+		return paperService.DeletePaperDetails(id);
+	}
+}
